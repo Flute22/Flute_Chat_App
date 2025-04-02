2f2a3ff9aa8cd1fa6192f1c3786155baa3409cc9
khushalsinhmar <shimarsabb@gmail.com>
2025-04-03 01:01:57 +0530

    implement API error and response handling utilities

---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = ( requestHandler ) => {
+    return (req, res, next) => {
+        Promise 
+            .resolve( requestHandler(req, res, next) )
+            .catch( (err) => {
+                res
+                    .status(err.statusCode || 500)
+                    .json(
+                        {
+                            success: false,
+                            message: err.message || "Internal Server Error"
+                        }
+                    )
+            } ) ;
+    };
+};
+
+export { asyncHandler }
